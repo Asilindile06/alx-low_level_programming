ec0070afef96539ef128a0a69c0eaa31ea2f4068
Asilindile06 <asilindilelee@gmail.com>
2023-06-28 10:08:27 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _0-strcat.c - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_0-strcat.c(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
